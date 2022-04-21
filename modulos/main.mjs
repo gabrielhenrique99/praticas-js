d3f2b839fd7b636be8c3af834a30fbd4c8e4918e
gabrielhenrique99 <gabrielhenrique16041999@gmail.com>
2022-04-21 20:43:55 -0300

    exemplo de como utilizar modulos em js

---
+++ modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade,mostraCidade,mostraHobbie } from "./funcoes.mjs";
+
+console.log(mostraIdade("Gabriel", 23));
+console.log(mostraCidade("Gabriel","Iguatu-CE"));
+console.log(mostraHobbie("Gabriel","Programar"));
